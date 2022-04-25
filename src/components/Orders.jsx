9575b21e5c2c8f12154523a38f8b964ac4578970
crushyz007 <gracetyn2002@gmail.com>
2022-04-25 22:48:01 +0700

    test push

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import '../css/Orders.css'
 
 const Orders =(props)=>{
-    const { cartItems, onRemove } = props;
+    // const { cartItems, onRemove } = props;
     // const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
     // const totalPrice = itemsPrice;
+    const {productname,quantity,price} = props
+
+    const onRemove = (product) =>
+    {
+
+    }
 
     return(
         <div>
             <div className="Orders__BoxPos">
                 <thead className="Orders__FontBox">
-                    <tr className="Orders__Font ">การ์ดน้องเบบี้ไวเปอร์</tr>                
-                    <tr className="Orders__Font Font__Right">1</tr>
-                    <tr className="Orders__Font Font__Right2">500</tr>
+                    <tr className="Orders__Font ">{productname}การ์ดน้องเบบี้ไวเปอร์</tr>                
+                    <tr className="Orders__Font Font__Right">{quantity}1</tr>
+                    <tr className="Orders__Font Font__Right2">{price}500</tr>
                     <tr className="Orders__Font Font__Right3">
                         <button className="Orders__Btn">-</button>
                         {/* <input type="checkbox" id="total2" />
