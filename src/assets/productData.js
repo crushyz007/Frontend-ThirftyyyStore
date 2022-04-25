0c38bf74fd878e2452dfc694768a288c166852c0
crushyz007 <gracetyn2002@gmail.com>
2022-04-26 05:19:49 +0700

    add product data

---
+++ src/assets/productData.js
@@ -0,0 +1,137 @@
+const productData = {
+    products: [
+        {
+            name: "การ์ดไอรีน feel my rhythm",
+            description: "การ์ดไอรีนบั้มล่าสุด สภาพดีมากค่ะ",
+            catalog: "card",
+            detail: "card",
+            stock: "1",
+            price: "200",
+            discount: "0",
+            type: "goods",
+            img: ["pic/product1-1.jpg","pic/product1-2.jpg"],
+            time: "0",
+            queuing: "0"
+        },
+        {
+            name: "การ์ดเยริ feel my rhythm",
+            description: "การ์ดน้องเยริแหงนมองจากหน้าหลัง สวยอย่างกับนางฟ้า สภาพดี หยิบออกมาดูครั้งเดียวแล้วเก็บทันที",
+            catalog: "card",
+            detail: "card",
+            stock: "1",
+            price: "250",
+            discount: "0",
+            type: "goods",
+            img:["pic/product2-1.jpg","pic/product2-2.jpg"],
+            time: "0",
+            queuing: "0"
+
+        },
+        {
+            name: "การ์ดซองฮุน OFFICIAL MD GOODS LIGHT STICK",
+            description: "มีซองฮุน MD GOODSค่ะ ขอแลกเป็นน้องซอนอูการ์ดหมอนค่ะ",
+            catalog: "card",
+            detail: "card",
+            stock: "1",
+            price: "0",
+            discount: "0",
+            type: "trade",
+            img:["pic/product3-1.jpg","pic/product3-2.jpg"],
+            time: "0",
+            queuing: "0",
+        },
+        {
+            name: "การ์ดน้องซอนอูลูกเทพ",
+            description: "ใครเป็นหม่าม้าน้องซอนอูไม่มีไม่ได้นะ must have มาก",
+            catalog: "card",
+            detail: "card" ,
+            stock: "1",
+            price: "500",
+            discount: "0",
+            type: "goods",
+            img: ["pic/product4-1.jpg","pic/product4-2.jpg"],
+            time: "0",
+            queuing: "0"
+        },
+        {
+            name: "อัลบั้ม Wanna One Power Of Destiny",
+            description: "สภาพ 95% ข้างในเป็นการ์ดควานลินและทิกเก็ตการ์ดพี่อง",
+            catalog: "album",
+            detail: "album",
+            stock: "1",
+            price: "300",
+            discount: "0",
+            type: "goods",
+            img:["pic/product5-1.jpg","pic/product5-2.jpg","pic/product5-3.jpg","pic/product5-4.jpg"],
+            time:"0",
+            queuing:"0"
+        },
+        {
+            name: "ตุ๊กตาโมชิเนโกมะ",
+            description:" สุดจะเหล่ท่อ ไม่เคยแกะออกจากซองเลย หายากแล้วนะตัวนี้ บิดทีละ50นะคะ",
+            catalog: "doll",
+            detail: "doll",
+            stock: "1",
+            price: "100",
+            discount: "0",
+            type: "auction",
+            img:["pic/product6-1.jpg","pic/product6-2.jpg"],
+            time: "24.00.00hrs",
+            queuing: "0"
+        },
+        {
+            name:"ผ้าคลุมหน่วยสำรวจ ",
+            description: "จำลองตนเองเป็นรีไวล์เฮย์โจว แล้ววิ่งไล่ฟันคอคน เท่ ๆ เคยใส่ไปครั้งเดียวถ้วน ซักให้แล้ว",
+            catalog: "cloth",
+            detail: "cloth",
+            stock: "1",
+            price: "1200",
+            discount: "0",
+            type: "goods",
+            img:["pic/product7-1.jpg","pic/product7-2.jpg"],
+            time: "0",
+            queuing: "0"
+        },
+        {
+            name: "แท่งไฟ Black Pink",
+            description: "เลิกติ่งแล้วค่ะ โละทิ้ง บิดที่ 50 บาทค่ะ",
+            catalog: "lightstick",
+            detail: "lightstick",
+            stock: "1",
+            price: "300",
+            discount: "0",
+            type: "auction",
+            img: ["pic/product8-1.jpg","pic/product8-2.jpg"],
+            time: "24.00.00hrs",
+            queuing: "0"
+        },
+        {
+            name: "โมเดลหินเทสเซอแรค",
+            description: "เทสเซอแรคก้อนสุดท้ายจากจักรวาลมาเวร",
+            catalog: "figure",
+            detail: "figure",
+            stock: "1",
+            price: "2000",
+            discount: "0",
+            type: "goods",
+            img:["pic/product9-1.jpg","pic/product9-2.jpg","pic/product9-3.jpg"],
+            time: "0",
+            queuing: "0"
+        },
+        {
+            name: "การ์ดน้องหนาว Aespa 2021 SMtown SMCU Express Genuine Kpop",
+            description: "ขอแลกเป็นคาริน่า บั้ม Black Mamba ค่ะ",
+            catalog: "card",
+            detail: "card",
+            stock: "1",
+            price: "0",
+            discount: "0",
+            type: "trade",
+            img:["pic/product10-1.jpg","pic/product10-2.jpg","pic/product10-3.jpg"],
+            time: "0",
+            queuing : "0"
+        }
+    ]
+
+}
+export default productData;
